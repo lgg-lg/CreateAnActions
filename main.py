16ffcf4f87ba7daa624d59265a950b327e827440
lgg-lg <lingganguligu@outlook.com>
2025-10-19 22:49:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import os
 
 def main():
     # 1. 检查文件是否存在
-    input_file = os.path.join("Data", "clean_titanic.csv")
-    if not os.path.exists(input_file):
+    file_path = os.path.join("Data", "clean_titanic.csv")
+    if not os.path.exists(file_path):
         print(f"错误：文件 {file_path} 不存在！")
         return
 
